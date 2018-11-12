ab7a4129a211e4bf01ea9462595dd7c50c81de31
SoftheonHXia <40275617+SoftheonHXia@users.noreply.github.com>
2018-11-12 15:15:48 -0500

    fixed Fiters failing to display values

--- frontend/src/metabase/components/TokenField.jsx
+++ frontend/src/metabase/components/TokenField.jsx
@@ -582,7 +582,7 @@ export default class TokenField extends Component {
         </li>
       </ul>
     );
-
+ 
     const optionsList =
       filteredOptions.length === 0 ? null : (
         <ul
@@ -592,7 +592,7 @@ export default class TokenField extends Component {
           onMouseLeave={() => this.setState({ listIsHovered: false })}
         >
           {filteredOptions.map(option => (
-            <li className="mr1" key={option.value.id}>
+            <li className="mr1" key={option.value}>
               <div
                 ref={
                   this._valueIsEqual(selectedOptionValue, this._value(option))
